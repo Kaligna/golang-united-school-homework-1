33a0468df1c55fde708d7cbaa56bcaaccabd9e56
Kaligna <99505705+Kaligna@users.noreply.github.com>
2022-02-17 23:46:11 +0600

    Commit

--- solution.go
+++ solution.go
@@ -1,5 +1,10 @@
 package solution
 
-func GetMessage() string {
-	return ""
-}
+import (
+		
+	"github.com/kyokomi/emoji/v2"
+)
+func main() {
+	Message := emoji.Sprint("Hello :map:!")
+	emoji.Println(Message)
+	}
